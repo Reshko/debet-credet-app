50127d911312b4a7868268fb003b8dc228bda36e
Aleksey Kolpakov <kolpak.al6@yandex.ru>
2022-09-04 20:21:48 +0300

    Lesson 008 Изменил реализацию приложения на StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,34 +2,68 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyFirstStatelessWidget()
+    // MaterialApp(
+    //   debugShowCheckedModeBanner: false,
+    //   home: Scaffold(
+    //     backgroundColor: Colors.brown[200],
+    //     appBar: AppBar(
+    //       title: const Text(
+    //         "Accounts",
+    //         style: TextStyle(color: Colors.black),
+    //       ),
+    //       backgroundColor: Colors.white,
+    //     ),
+    //     body: Center(
+    //       child: Column(
+    //         children: [
+    //           Text("asdasd"),
+    //           Text("tesat")
+    //         ],
+    //       ),
+    //       // child: Text(
+    //       //   "Hello Flutter",
+    //       //   style: TextStyle(
+    //       //     fontSize: 40,
+    //       //     color: Colors.white,
+    //       //     fontStyle: FontStyle.italic,
+    //       //   ),
+    //       // ),
+    //     ),
+    //   )
+    // )
+  );
+}
+
+
+class MyFirstStatelessWidget extends StatelessWidget {
+  build(BuildContext context){
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.brown[200],
-        appBar: AppBar(
-          title: const Text(
-            "Accounts",
-            style: TextStyle(color: Colors.black),
-          ),
-          backgroundColor: Colors.white,
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              Text("asdasd"),
-              Text("tesat")
+        appBar:  AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const <Widget>[
+              FloatingActionButton(
+                onPressed: null,
+                child: Icon(Icons.more_horiz),
+              ),
+              FloatingActionButton(
+                onPressed: null,
+                child: Icon(Icons.add),
+              ),
             ],
-          ),
-          // child: Text(
-          //   "Hello Flutter",
-          //   style: TextStyle(
-          //     fontSize: 40,
-          //     color: Colors.white,
-          //     fontStyle: FontStyle.italic,
-          //   ),
-          // ),
+          ) 
         ),
-      )
-    )
-  );
+        body: const Text(
+          "Accounts",
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+      ),
+    );
+  }
 }
